339603671663506b269a6c72ffef1b997ceece40
Mushfiqur-Rahman-Robin <u1604123@student.cuet.ac.bd>
2025-08-10 00:49:12 +0600

    feat: maps in go

---
+++ ch_9/maps.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"io"
+	"sort"
+)
+
+func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
+	users := make(map[string]user)
+	if len(names) != len(phoneNumbers) {
+		return nil, errors.New("invalid sizes")
+	}
+	for i := range names {
+		name := names[i]
+		users[name] = user{
+			name:        name,
+			phoneNumber: phoneNumbers[i],
+		}
+	}
+	return users, nil
+}
+
+// mutating maps
+func deleteIfNecessary(users map[string]user2, name string) (deleted bool, err error) {
+	user, ok := users[name]
+	if !ok {
+		return false, errors.New("not found")
+	}
+	if !user.scheduledForDeletion {
+		return false, nil
+	}
+	delete(users, name)
+	return true, nil
+}
+
+type user struct {
+	name        string
+	phoneNumber int
+}
+
+type user2 struct {
+	name                 string
+	number               int
+	scheduledForDeletion bool
+}
+
+// maps count
+func getCounts(userIDs []string) map[string]int {
+	m := map[string]int{}
+	for _, id := range userIDs {
+		if _, ok := m[id]; !ok {
+			m[id] = 0
+		}
+		m[id]++
+	}
+	return m
+}
+
+// nested maps
+func getNameCounts(names []string) map[rune]map[string]int {
+	nameCounts := make(map[rune]map[string]int)
+	for _, name := range names {
+		firstLetter := rune(0)
+		if len(name) != 0 {
+			firstLetter = rune(name[0])
+		}
+
+		if _, ok := nameCounts[firstLetter]; !ok {
+			nameCounts[firstLetter] = make(map[string]int)
+		}
+		if _, ok := nameCounts[firstLetter][name]; !ok {
+			nameCounts[firstLetter][name] = 0
+		}
+		nameCounts[firstLetter][name]++
+	}
+	return nameCounts
+}
+
+func getNames(length int) []string {
+	names := []string{
+		"Grant", "Eduardo", "Peter", "Matthew", "Matthew", "Matthew", "Peter", "Peter", "Henry", "Parker", "Parker", "Parker", "Collin", "Hayden", "George", "Bradley", "Mitchell", "Devon", "Ricardo", "Shawn", "Taylor", "Nicolas", "Gregory", "Francisco", "Liam", "Kaleb", "Preston", "Erik", "Alexis", "Owen", "Omar", "Diego", "Dustin", "Corey", "Fernando", "Clayton", "Carter", "Ivan", "Jaden", "Javier", "Alec", "Johnathan", "Scott", "Manuel", "Cristian", "Alan", "Raymond", "Brett", "Max", "Drew", "Andres", "Gage", "Mario", "Dawson", "Dillon", "Cesar", "Wesley", "Levi", "Jakob", "Chandler", "Martin", "Malik", "Edgar", "Sergio", "Trenton", "Josiah", "Nolan", "Marco", "Drew", "Peyton", "Harrison", "Drew", "Hector", "Micah", "Roberto", "Drew", "Brady", "Erick", "Conner", "Jonah", "Casey", "Jayden", "Edwin", "Emmanuel", "Andre", "Phillip", "Brayden", "Landon", "Giovanni", "Bailey", "Ronald", "Braden", "Damian", "Donovan", "Ruben", "Frank", "Gerardo", "Pedro", "Andy", "Chance", "Abraham", "Calvin", "Trey", "Cade", "Donald", "Derrick", "Payton", "Darius", "Enrique", "Keith", "Raul", "Jaylen", "Troy", "Jonathon", "Cory", "Marc", "Eli", "Skyler", "Rafael", "Trent", "Griffin", "Colby", "Johnny", "Chad", "Armando", "Kobe", "Caden", "Marcos", "Cooper", "Elias", "Brenden", "Israel", "Avery", "Zane", "Zane", "Zane", "Zane", "Dante", "Josue", "Zackary", "Allen", "Philip", "Mathew", "Dennis", "Leonardo", "Ashton", "Philip", "Philip", "Philip", "Julio", "Miles", "Damien", "Ty", "Gustavo", "Drake", "Jaime", "Simon", "Jerry", "Curtis", "Kameron", "Lance", "Brock", "Bryson", "Alberto", "Dominick", "Jimmy", "Kaden", "Douglas", "Gary", "Brennan", "Zachery", "Randy", "Louis", "Larry", "Nickolas", "Albert", "Tony", "Fabian", "Keegan", "Saul", "Danny", "Tucker", "Myles", "Damon", "Arturo", "Corbin", "Deandre", "Ricky", "Kristopher", "Lane", "Pablo", "Darren", "Jarrett", "Zion", "Alfredo", "Micheal", "Angelo", "Carl", "Oliver", "Kyler", "Tommy", "Walter", "Dallas", "Jace", "Quinn", "Theodore", "Grayson", "Lorenzo", "Joe", "Arthur", "Bryant", "Roman", "Brent", "Russell", "Ramon", "Lawrence", "Moises", "Aiden", "Quentin", "Jay", "Tyrese", "Tristen", "Emanuel", "Salvador", "Terry", "Morgan", "Jeffery", "Esteban", "Tyson", "Braxton", "Branden", "Marvin", "Brody", "Craig", "Ismael", "Rodney", "Isiah", "Marshall", "Maurice", "Ernesto", "Emilio", "Brendon", "Kody", "Eddie", "Malachi", "Abel", "Keaton", "Jon", "Shaun", "Skylar", "Ezekiel", "Nikolas", "Santiago", "Kendall", "Axel", "Camden", "Trevon", "Bobby", "Conor", "Jamal", "Lukas", "Malcolm", "Zackery", "Jayson", "Javon", "Roger", "Reginald", "Zachariah", "Desmond", "Felix", "Johnathon", "Dean", "Quinton", "Ali", "Davis", "Gerald", "Rodrigo", "Demetrius", "Billy", "Rene", "Reece", "Kelvin", "Leo", "Justice", "Chris", "Guillermo", "Matthew", "Matthew", "Matthew", "Kevon", "Steve", "Frederick", "Clay", "Weston", "Dorian", "Hugo", "Roy", "Orlando", "Terrance", "Kai", "Khalil", "Khalil", "Khalil", "Graham", "Noel", "Willie", "Nathanael", "Terrell", "Tyrone",
+	}
+	if length > len(names) {
+		length = len(names)
+	}
+	return names[:length]
+}
+
+func test(names []string, phoneNumbers []int) {
+	fmt.Println("Creating map...")
+	defer fmt.Println("====================================")
+	users, err := getUserMap(names, phoneNumbers)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, name := range names {
+		fmt.Printf("key: %v, value:\n", name)
+		fmt.Println(" - name:", users[name].name)
+		fmt.Println(" - number:", users[name].phoneNumber)
+	}
+}
+
+func testMutatedMap(users map[string]user2, name string) {
+	fmt.Printf("Attempting to delete %s...\n", name)
+	defer fmt.Println("====================================")
+	deleted, err := deleteIfNecessary(users, name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if deleted {
+		fmt.Println("Deleted:", name)
+		return
+	}
+	fmt.Println("Did not delete:", name)
+}
+
+func testMapCount(userIDs []string, ids []string) {
+	fmt.Printf("Generating counts for %v user IDs...\n", len(userIDs))
+
+	counts := getCounts(userIDs)
+	fmt.Println("Counts from select IDs:")
+	for _, k := range ids {
+		v := counts[k]
+		fmt.Printf(" - %s: %d\n", k, v)
+	}
+	fmt.Println("=====================================")
+}
+
+func testNestedMap(names []string, initial rune, name string) {
+	fmt.Printf("Generating counts for %v names...\n", len(names))
+
+	nameCounts := getNameCounts(names)
+	count := nameCounts[initial][name]
+	fmt.Printf("Count for [%c][%s]: %d\n", initial, name, count)
+	fmt.Println("=====================================")
+}
+
+func main() {
+	test(
+		[]string{"John", "Bob", "Jill"},
+		[]int{14355550987, 98765550987, 18265554567},
+	)
+	test(
+		[]string{"John", "Bob"},
+		[]int{14355550987, 98765550987, 18265554567},
+	)
+	test(
+		[]string{"George", "Sally", "Rich", "Sue"},
+		[]int{20955559812, 38385550982, 48265554567, 16045559873},
+	)
+
+	users := map[string]user2{
+		"john": {
+			name:                 "john",
+			number:               18965554631,
+			scheduledForDeletion: true,
+		},
+		"elon": {
+			name:                 "elon",
+			number:               19875556452,
+			scheduledForDeletion: true,
+		},
+		"breanna": {
+			name:                 "breanna",
+			number:               98575554231,
+			scheduledForDeletion: false,
+		},
+		"kade": {
+			name:                 "kade",
+			number:               10765557221,
+			scheduledForDeletion: false,
+		},
+	}
+	testMutatedMap(users, "john")
+	testMutatedMap(users, "musk")
+	testMutatedMap(users, "santa")
+	testMutatedMap(users, "kade")
+
+	keys := []string{}
+	for name := range users {
+		keys = append(keys, name)
+	}
+	sort.Strings(keys)
+
+	fmt.Println("Final map keys:")
+	for _, name := range keys {
+		fmt.Println(" - ", name)
+	}
+
+	userIDs := []string{}
+	for i := 0; i < 10000; i++ {
+		h := md5.New()
+		io.WriteString(h, fmt.Sprint(i))
+		key := fmt.Sprintf("%x", h.Sum(nil))
+		userIDs = append(userIDs, key[:2])
+	}
+
+	testMapCount(userIDs, []string{"00", "ff", "dd"})
+	testMapCount(userIDs, []string{"aa", "12", "32"})
+	testMapCount(userIDs, []string{"bb", "33"})
+
+	testNestedMap(getNames(50), 'M', "Matthew")
+	testNestedMap(getNames(100), 'G', "George")
+	testNestedMap(getNames(150), 'D', "Drew")
+	testNestedMap(getNames(200), 'P', "Philip")
+	testNestedMap(getNames(250), 'B', "Bryant")
+	testNestedMap(getNames(300), 'M', "Matthew")
+
+}
