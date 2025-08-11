3b845097ba118a3f4b3836a804ed120c2b0dd790
Mushfiqur-Rahman-Robin <u1604123@student.cuet.ac.bd>
2025-08-11 08:58:44 +0600

    feat: channels in go

---
+++ ch_13/coroutine.go
@@ -0,0 +1,343 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func sendEmail(message string) {
+	go func() {
+		time.Sleep(time.Millisecond * 250)
+		fmt.Printf("Email received: '%s'\n", message)
+	}()
+	fmt.Printf("Email sent: '%s'\n", message)
+}
+
+// deadlock and channel
+func filterOldEmails(emails []email) {
+	isOldChan := make(chan bool)
+
+	go func() {
+		for _, e := range emails {
+			if e.date.Before(time.Date(2020, 0, 0, 0, 0, 0, 0, time.UTC)) {
+				isOldChan <- true
+				continue
+			}
+			isOldChan <- false
+		}
+	}()
+
+	isOld := <-isOldChan
+	fmt.Println("email 1 is old:", isOld)
+	isOld = <-isOldChan
+	fmt.Println("email 2 is old:", isOld)
+	isOld = <-isOldChan
+	fmt.Println("email 3 is old:", isOld)
+}
+
+type email struct {
+	body string
+	date time.Time
+}
+
+// empty struct
+func waitForDbs(numDBs int, dbChan chan struct{}) {
+	for i := 0; i < numDBs; i++ {
+		<-dbChan // this is a blocking call until the channel is closed
+	}
+}
+
+func getDatabasesChannel(numDBs int) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		for i := 0; i < numDBs; i++ {
+			ch <- struct{}{}
+			fmt.Printf("Database %v is online\n", i+1)
+		}
+	}()
+	return ch
+}
+
+// buffer channel
+func addEmailsToQueue(emails []string) chan string {
+	emailsToSend := make(chan string, len(emails))
+	for _, email := range emails {
+		emailsToSend <- email
+	}
+	return emailsToSend
+}
+
+func sendEmails(batchSize int, ch chan string) {
+	for i := 0; i < batchSize; i++ {
+		email := <-ch
+		fmt.Println("Sending email:", email)
+	}
+}
+
+// close channel
+func countReports(numSentCh chan int) int {
+	total := 0
+	for {
+		numSent, ok := <-numSentCh
+		if !ok {
+			break
+		}
+		total += numSent
+	}
+	return total
+}
+
+func sendReports(numBatches int, ch chan int) {
+	for i := 0; i < numBatches; i++ {
+		numReports := i*23 + 32%17
+		ch <- numReports
+		fmt.Printf("Sent batch of %v reports\n", numReports)
+		time.Sleep(time.Millisecond * 100)
+	}
+	close(ch)
+}
+
+// channel range
+func concurrrentFib(n int) {
+	ch := make(chan int)
+	go fibonacci(n, ch)
+
+	for v := range ch {
+		fmt.Println(v)
+	}
+}
+
+func fibonacci(n int, ch chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		ch <- x
+		x, y = y, x+y
+		time.Sleep(time.Millisecond * 10)
+	}
+	close(ch)
+}
+
+// select statement for channel
+func logMessages(chEmails, chSms chan string) {
+	for {
+		select {
+		case email, ok := <-chEmails:
+			if !ok {
+				return
+			}
+			logEmail((email))
+		case sms, ok := <-chSms:
+			if !ok {
+				return
+			}
+			logSms(sms)
+		}
+	}
+}
+
+func logSms(sms string) {
+	fmt.Println("SMS:", sms)
+}
+
+func logEmail(email string) {
+	fmt.Println("Email:", email)
+}
+
+func sendToLogger(sms, emails []string) (chSms, chEmails chan string) {
+	chSms = make(chan string)
+	chEmails = make(chan string)
+	go func() {
+		for i := 0; i < len(sms) && i < len(emails); i++ {
+			done := make(chan struct{})
+			s := sms[i]
+			e := emails[i]
+			t1 := time.Millisecond * time.Duration(rand.Intn(1000))
+			t2 := time.Millisecond * time.Duration(rand.Intn(1000))
+			go func() {
+				time.Sleep(t1)
+				chSms <- s
+				done <- struct{}{}
+			}()
+			go func() {
+				time.Sleep(t2)
+				chEmails <- e
+				done <- struct{}{}
+			}()
+			<-done
+			<-done
+			time.Sleep(10 * time.Millisecond)
+		}
+		close(chSms)
+		close(chEmails)
+	}()
+	return chSms, chEmails
+}
+
+// select with default
+func saveBackups(snapshotTicker, saveAfter <-chan time.Time) {
+	for {
+		select {
+		case <-snapshotTicker:
+			takeSnapshot()
+		case <-saveAfter:
+			saveSnapshot()
+			return
+		default:
+			waitForData()
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
+
+func takeSnapshot() {
+	fmt.Println("Taking a backup snapshot...")
+}
+
+func saveSnapshot() {
+	fmt.Println("All backups saved!")
+}
+
+func waitForData() {
+	fmt.Println("Nothing to do, waiting...")
+}
+
+func testSelectWithDefault() {
+	snapshotTicker := time.Tick(800 * time.Millisecond)
+	saveAfter := time.After(2800 * time.Millisecond)
+	saveBackups(snapshotTicker, saveAfter)
+	fmt.Println("===========================")
+}
+
+func testSelect(sms []string, emails []string) {
+	fmt.Println("Starting...")
+
+	chSms, chEmails := sendToLogger(sms, emails)
+
+	logMessages(chEmails, chSms)
+	fmt.Println("===============================")
+}
+
+func testRange(n int) {
+	fmt.Printf("Printing %v numbers...\n", n)
+	concurrrentFib(n)
+	fmt.Println("==============================")
+}
+
+func testClose(numBatches int) {
+	numSentCh := make(chan int)
+	go sendReports(numBatches, numSentCh)
+
+	fmt.Println("Start counting...")
+	numReports := countReports(numSentCh)
+	fmt.Printf("%v reports sent!\n", numReports)
+	fmt.Println("========================")
+}
+
+func testBuffer(emails ...string) {
+	fmt.Printf("Adding %v emails to queue...\n", len(emails))
+	ch := addEmailsToQueue(emails)
+	fmt.Println("Sending emails...")
+	sendEmails(len(emails), ch)
+	fmt.Println("==========================================")
+}
+
+func testEmptyStruct(numDBs int) {
+	dbChan := getDatabasesChannel(numDBs)
+	fmt.Printf("Waiting for %v databases...\n", numDBs)
+	waitForDbs(numDBs, dbChan)
+	time.Sleep(time.Millisecond * 10) // ensure the last print statement happens
+	fmt.Println("All databases are online!")
+	fmt.Println("=====================================")
+}
+
+func test(message string) {
+	sendEmail(message)
+	time.Sleep(time.Millisecond * 500)
+	fmt.Println("========================")
+}
+
+func testChannel(emails []email) {
+	filterOldEmails(emails)
+	fmt.Println("==========================================")
+}
+
+func main() {
+	test("Hello there Stacy!")
+	test("Hi there John!")
+	test("Hey there Jane!")
+
+	testChannel([]email{
+		{
+			body: "Are you going to make it?",
+			date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			body: "I need a break",
+			date: time.Date(2021, 0, 0, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			body: "What were you thinking?",
+			date: time.Date(2022, 0, 0, 0, 0, 0, 0, time.UTC),
+		},
+	})
+	testChannel([]email{
+		{
+			body: "Yo are you okay?",
+			date: time.Date(2018, 0, 0, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			body: "Have you heard of that website Boot.dev?",
+			date: time.Date(2017, 0, 0, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			body: "It's awesome honestly.",
+			date: time.Date(2016, 0, 0, 0, 0, 0, 0, time.UTC),
+		},
+	})
+
+	testEmptyStruct(3)
+	testEmptyStruct(5)
+	testEmptyStruct(10)
+
+	testBuffer("Hello John, tell Kathy I said hi", "Whazzup bruther")
+	testBuffer("I find that hard to believe.", "When? I don't know if I can", "What time are you thinking?")
+	testBuffer("She says hi!", "Yeah its tomorrow. So we're good.", "Cool see you then!", "Bye!")
+
+	testClose(3)
+	testClose(5)
+	testClose(10)
+
+	testRange(3)
+	testRange(5)
+	testRange(10)
+
+	testSelect(
+		[]string{
+			"hi friend",
+			"What's going on?",
+			"Welcome to the business",
+			"I'll pay you to be my friend",
+		},
+		[]string{
+			"Will you make your appointment?",
+			"Let's be friends",
+			"What are you doing?",
+			"I can't believe you've done this.",
+		},
+	)
+	testSelect(
+		[]string{
+			"this song slaps hard",
+			"yooo hoooo",
+			"i'm a big fan",
+		},
+		[]string{
+			"What do you think of this song?",
+			"I hate this band",
+			"Can you believe this song?",
+		},
+	)
+
+	testSelectWithDefault()
+}
